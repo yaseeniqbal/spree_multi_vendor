55603587ad301a56057206eb91c3840deebd5958
Usman <usman@Omairs-MacBook-Air.local>
2019-04-23 21:13:33 +0500

    added description in vendor table

--- config/initializers/spree_permitted_attributes.rb
+++ config/initializers/spree_permitted_attributes.rb
@@ -6,6 +6,6 @@ module Spree
 
     @@vendor_attributes = [:name, :about_us, :contact_us,:t_distance,:address,:lat,:lng,:is_appointable,:logo_image,
                            :email, :speciality, :google_address_url, :opn_hrs, :mon_hrs, :tue_hrs, :wed_hrs, :thu_hrs, 
-                           :fri_hrs, :sat_hrs, :sun_hrs, :pub_holiday_hrs, :fb_lnk, :pintrest_lnk, :twitter_lnk, :whatsapp_lnk]
+                           :fri_hrs, :sat_hrs, :sun_hrs, :pub_holiday_hrs, :fb_lnk, :pintrest_lnk, :twitter_lnk, :whatsapp_lnk, :description]
   end
 end
