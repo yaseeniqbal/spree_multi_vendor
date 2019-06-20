de6cab6bc7fab4ff6d13cf5852982e5c485400af
usmanriaz111 <l124061@lhr.nu.edu.pk>
2019-06-20 19:58:31 +0500

    Updated the get_product_ fund

--- app/controllers/spree/vendors_controller.rb
+++ app/controllers/spree/vendors_controller.rb
@@ -16,8 +16,9 @@ module Spree
 
     end
     def get_store_products
-      @products = Spree::Vendor.friendly.find(params[:id]).products.approved.in_stock
-      @title = params[:id]
+      store = Spree::Vendor.friendly.find(params[:id])
+      @products = store.products.approved.in_stock
+      @title = store.name
     end
 
     private
