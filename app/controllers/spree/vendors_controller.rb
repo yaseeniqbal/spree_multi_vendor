f771f71b1f10aacb8f3c1c97022ceacf72580171
usmanriaz111 <l124061@lhr.nu.edu.pk>
2019-07-24 19:39:16 +0500

    Updated the multi vendor pages pagination

--- app/controllers/spree/vendors_controller.rb
+++ app/controllers/spree/vendors_controller.rb
@@ -3,12 +3,19 @@ module Spree
     before_action :set_spree_vendor, only: [:show]
 
     def index
+      @searcher = build_searcher(params.merge(include_images: true))
+      @stores = @searcher.retrieve_vendors
       if params[:keywords].present?
         params[:q] = {name_cont: params[:keywords]}
-        @q = Spree::Vendor.ransack(params[:q])
-        @stores = @q.result(distinct: true).approved
-      else
-        @stores = Spree::Vendor.approved
+                         # .page(curr_page).per(per_page)
+        @q = @stores.ransack(params[:q])
+        @stores = @q.result(distinct: true)
+      # else
+      #   @stores = Spree::Vendor.approved
+      end
+      respond_to do |format|
+        format.js
+        format.html
       end
     end
 
@@ -17,7 +24,15 @@ module Spree
     end
     def get_store_products
       store = Spree::Vendor.friendly.find(params[:id])
-      @products = store.products.approved.in_stock.distinct
+      @searcher = build_searcher(params.merge(include_images: true))
+      if @searcher.properties
+        per_page = @searcher.properties[:per_page]
+        page = @searcher.properties[:page]
+        curr_page = page || 1
+        @products = store.products.page(curr_page).per(per_page).approved.in_stock.distinct
+      else
+        @products = store.products.approved.in_stock.distinct
+      end
       @title = store.name
       if params[:store_slug].present?
         @store_slug = params[:store_slug]
